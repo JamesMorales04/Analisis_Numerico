4fae1c4de9bcfc458a656010a0bafe7403fc98de
friosl <friosl@eafit.edu.co>
2020-04-13 12:30:32 +0200

    Newton act

--- Proyecto_Final/Ecuaciones_no_lineales/Newton.py
+++ Proyecto_Final/Ecuaciones_no_lineales/Newton.py
@@ -2,33 +2,32 @@ from Funciones import *
 import math
 class Newton:
     def __init__(self):
-        self.valores=[]
+        self.values=[]
         self.raiz=""
 
-    def algoritmo_newton(self, xi, Funcion,iteraciones, tolerancia, tipo_de_error):
-        print(tipo_de_error)
+    def algoritmo_newton(self, xi, Funcion,iterations, tolerance, err_type):
+        print(err_type)
         xi_1 = xi
-        contador = 1
-        error = tolerancia+1
+        counter = 1
+        error = tolerance+1
         fx=Funcion.evaluar(xi)
         dfx=Funcion.evaluar_derivada(xi)
-        #Aqui deberia hacer el parse 
-        self.valores.append([contador, xi, fx,dfx, error])
-        while ((error > tolerancia) and (fx != 0) and (dfx!=0) and (contador < iteraciones)):
+        self.values.append([counter, xi, fx,dfx, error])
+        while ((error > tolerance) and (fx != 0) and (dfx!=0) and (counter < iterations)):
             xi_1 = xi_1-(Funcion.evaluar(xi)/Funcion.evaluar_derivada(xi))
             fx=Funcion.evaluar(xi_1)
             dfx=Funcion.evaluar_derivada(xi)
-            if(tipo_de_error):
+            if(err_type):
                 error = math.fabs((xi - xi_1)/ xi)
             else:
                 error = math.fabs(xi - xi_1)
             xi = xi_1
-            contador+=1
-            self.valores.append([contador, xi, fx,dfx, error])
+            counter+=1
+            self.values.append([counter, xi, fx,dfx, error])
 
         if(fx == 0):
             self.raiz=f"[{xi} is a root]"
-        elif (error<tolerancia):
+        elif (error<tolerance):
             self.raiz=f"[{xi} is an approximated root]"
         elif(dfx == 0):
             self.raiz=f"[{xi} is possibly a multiple root]"
@@ -36,6 +35,6 @@ class Newton:
             self.raiz=f"Exceeded iterations"
 
     def tabla_valores(self):
-        return self.valores
+        return self.values
     def get_sol(self):
         return str(self.raiz)
