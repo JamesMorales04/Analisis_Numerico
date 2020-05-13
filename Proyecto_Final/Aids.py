998a4fb80b5cb4a35381eeeef4386838c8826060
friosl <friosl@eafit.edu.co>
2020-05-13 17:46:59 -0500

    Muchos nombres

--- Proyecto_Final/Aids.py
+++ Proyecto_Final/Aids.py
@@ -1,35 +1,35 @@
-class Ayudas:
+class Aids:
 
     def help_busqueda(self):
-        data=open("Ayudas\Requisites_is.txt", "r")
+        data=open("Aids\Requisites_is.txt", "r")
         return data.read()
         
     def help_biseccion(self):
-        data=open("Ayudas\Requisites_bisection.txt", "r")
+        data=open("Aids\Requisites_bisection.txt", "r")
         return data.read()
 
     def help_regla_falsa(self):
-        data=open("Ayudas\Requisites_regulifalsi.txt", "r")
+        data=open("Aids\Requisites_regulifalsi.txt", "r")
         return data.read()
 
     def help_punto_fijo(self):
-        data=open("Ayudas\ExplicacionPuntoFijo.txt", "r")
+        data=open("Aids\ExplicacionPuntoFijo.txt", "r")
         return data.read()
 
     def help_newton(self):
-        data=open("Ayudas\\NewtonMethodExplanation.txt", "r")
+        data=open("Aids\\NewtonMethodExplanation.txt", "r")
         return data.read()
 
     def help_secante(self):
-        data=open("Ayudas\\SecantMethodExplanation.txt", "r")
+        data=open("Aids\\SecantMethodExplanation.txt", "r")
         return data.read()
 
     def help_raices_multiples(self):
-        data=open("Ayudas\\MultipleRootsMethodExplanation.txt", "r")
+        data=open("Aids\\MultipleRootsMethodExplanation.txt", "r")
         return data.read()
 
     def help_gaussian_elimination():
-        data=open("Ayudas\\NonLinearEq\\GaussianEliminationMethodExplanation.txt", "r")
+        data=open("Aids\\NonLinearEq\\GaussianEliminationMethodExplanation.txt", "r")
 
     def help_partial_pivot():
-        data=open("Ayudas\\NonLinearEq\\partialPivotMethodExplanation.txt", "r")      
+        data=open("Aids\\NonLinearEq\\partialPivotMethodExplanation.txt", "r")      
