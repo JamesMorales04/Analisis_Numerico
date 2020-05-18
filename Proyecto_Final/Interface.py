9f895ca1659b6e637d8169223cb9c82882878765
friosl <friosl@eafit.edu.co>
2020-05-17 20:20:16 -0500

    .

--- Proyecto_Final/Interface.py
+++ Proyecto_Final/Interface.py
@@ -430,7 +430,6 @@ class matrix_Factorization_direct_doolitle(Screen):
         table=Tables()
         verify=Verify()
         error=""
-        print("agua pas´or xd")
         if(error==""):
             matrixb_clean=self.clean((self.matrixb.text).split("\n"))
             matrix_clean=self.clean((self.matrix.text).split("\n"))
