2aca28e7232cbda5cade370e5a56446d75a328ed
pquinterol <pquinterol@eafit.edu.co>
2020-05-21 21:43:26 -0500

    Choleskys output fix. God damn, im tired

--- Proyecto_Final/Systems_of_equations/Cholesky.py
+++ Proyecto_Final/Systems_of_equations/Cholesky.py
@@ -16,10 +16,15 @@ class Cholesky:
         self.resultX = []
         self.resultZ = []
         self.rows = []
+        getcontext().prec = 25
 
     def cholesky_algorithm(self,matrixA,matrixB =[]):
         
-        self.original = copy.deepcopy(matrixA)
+        print('MatrixA')
+        print(matrixA)
+        print('MatrixB')
+        print(matrixB)
+        self.original = copy.deepcopy(self.__merge(matrixA,matrixB))
         self.matrixL = [[0 for x in range(len(matrixA))] for y in range(len(matrixA))]
         self.matrixU = [[0 for x in range(len(matrixA))] for y in range(len(matrixA))]
         '''
@@ -36,30 +41,29 @@ class Cholesky:
         if(error==0):
             matrixLCopy = copy.deepcopy(self.matrixL)
             self.matrixLB = self.__merge(matrixLCopy,matrixB)
-            print('\nMatrixLB')
+            #print('\nMatrixLB')
             self.printMatrix(self.matrixLB)
             self.__getVarsValuesZ()
-            print('\nZ')
-            print(self.resultZ)
+            #print('\nZ')
+            #print(self.resultZ)
             matrixUCopy = copy.deepcopy(self.matrixU)
             self.matrixUZ = self.__merge(matrixUCopy,self.resultZ)
             
-            print('\nMatrixUZ')
+            #print('\nMatrixUZ')
             self.printMatrix(self.matrixUZ)
             
             self.__getVarsValuesX()
-            print('\nX')
-            print(self.resultX)
+            #print('\nX')
+            #print(self.resultX)
             
             #Factorization
-            print('\nMatrixL')
+            #print('\nMatrixL')
             self.printMatrix(self.matrixL)
-            print('\nMatrixU')
+            #print('\nMatrixU')
             self.printMatrix(self.matrixU)
 
-            self.row_definition(3)
-            print(self.rows)
-            print(self.get_results())
+            self.row_definition()
+            #print(len(self.rows))
 
         
         else:
@@ -136,12 +140,19 @@ class Cholesky:
 
         return matrixA
         
-    def row_definition(self,tables):
-        for i in range(0,tables):
-            for i in range(1,len(self.resultX)+1):
-                self.rows.append(f"x{i}")
-            self.rows.append("b")
-            self.rows.append("///")
+    def row_definition(self):
+        numRows = len(self.resultX)
+        for i in range(1,numRows+1):
+            self.rows.append(f"A{i}")
+        self.rows.append("B")
+        self.rows.append("///")
+        for i in range(1,numRows+1):
+            self.rows.append(f"L{i}")
+        self.rows.append("Z")
+        self.rows.append("///")
+        for i in range(1,numRows+1):
+            self.rows.append(f"U{i}")
+        self.rows.append("X")
 
     def value_table(self):
         for i in range(0,len(self.original)):
@@ -149,14 +160,13 @@ class Cholesky:
             for j in range(0,len(self.original[i])):
                 self.total[i].append(Decimal(self.original[i][j]))
             self.total[i].append("///")
-            for j in range(0,len(self.original[i])):
+            for j in range(0,len(self.matrixL)):
                 self.total[i].append(Decimal(self.matrixL[i][j]))
+            self.total[i].append(Decimal(self.resultZ[0][i]))
             self.total[i].append("///")    
-            for j in range(0,len(self.original[i])):
+            for j in range(0,len(self.matrixU)):
                 self.total[i].append(Decimal(self.matrixU[i][j]))
-            self.total[i].append("///")
-        for i in self.total:
-            print(i)
+            self.total[i].append(Decimal(self.resultX[i]))
         return self.total
     
     def get_results(self):
