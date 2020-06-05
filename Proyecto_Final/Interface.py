71fd13ab2c2e77a411f61f015ddf4f3af03fbd5a
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2020-06-04 22:51:33 -0500

    corrección

--- Proyecto_Final/Interface.py
+++ Proyecto_Final/Interface.py
@@ -719,7 +719,7 @@ class Interpolation_lagrange(Screen):
         matrix_clean=self.clean((self.matrix.text).split("\n"))        
         #error=verify.verify_length(matrix_clean)
         #error=verify.verify_length(matrixb_clean)
-        if(error):
+        if(matrix_clean and matrixb_clean):
             method=Lagrange()
             method.lagrange_interpol_algorithm(matrix_clean[0],matrixb_clean[0])
             response = method.getPolynomial()
