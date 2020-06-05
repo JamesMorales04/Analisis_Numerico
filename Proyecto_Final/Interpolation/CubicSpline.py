856efa0c3b5e34c5e91129351f6a66b07b0269dc
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2020-06-04 23:13:04 -0500

    x

--- Proyecto_Final/Interpolation/CubicSpline.py
+++ Proyecto_Final/Interpolation/CubicSpline.py
@@ -69,6 +69,8 @@ class CubicSpline:
     def check_values(self, ar,br):
         if not ar or not br:
             return False
+        elif len(ar[0]) != len(br[0]):
+            return False
         arr = ar[0]
         arr.sort()
         iter = 1
